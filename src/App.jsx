5eea4bed6fb6956deafa0d948ee397bde72099c1
Yashika soni <soniyashika164@gmail.com>
2026-02-11 21:26:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { Navbar } from './components/Navbar';
-import { Hero } from './components/Hero';
-import { HeroStatsBar } from './components/HeroStatsBar';
-import { FeaturesTabs } from './components/Features-tabs';
-import { DashboardPreview } from './components/DashboardPreview';
-import { ThemeProvider } from './components/theme-provider';
-import { Testimonials } from './components/Testimonials';
-import { Pricing } from './components/Pricing';
-import { Footer } from './components/Footer';
+import { Routes, Route } from "react-router-dom"
+import { Navbar } from "./components/Navbar"
+import { Footer } from "./components/Footer"
+import { HomePage } from "./pages/HomePage"
+import { ContactPage } from "./pages/ContactPage"
+import { SignupPage } from "./pages/SignupPage"
+import { LoginPage } from "./pages/LoginPage"
 
 function App() {
   return (
-    <ThemeProvider>
       <div className="min-h-screen  w-full overflow-x-hidden bg-gradient-to-b from-[#020617] via-[#020617] to-[#020617] text-white">
         <Navbar />
 
         {/* Give space below fixed navbar so content isn't hidden */}
-        <div className="pt-24 md:pt-28 space-y-24 md:space-y-32">
-          <Hero />
-          <HeroStatsBar />
-          <FeaturesTabs />
-          <DashboardPreview />
-          <Testimonials />
-          <Pricing />
-        </div>
+        <main className="pt-24 md:pt-28">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </main>
 
         <Footer />
       </div>
-    </ThemeProvider>
   )
 }
 
-export default App;
+export default App
